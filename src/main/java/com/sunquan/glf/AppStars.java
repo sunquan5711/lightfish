ee1125b15e8dd4b3b37c3bac3be6bb9e2a4cf707
sunquan <sunquan5711@outlook.com>
2019-05-06 17:47:41 +0800

    现在可以部署了

--- src/main/java/com/sunquan/glf/AppStars.java
+++ src/main/java/com/sunquan/glf/AppStars.java
@@ -2,18 +2,14 @@ package com.sunquan.glf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class AppStars extends SpringBootServletInitializer {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(AppStars.class,args);
-//    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(AppStars.class);
+    public static void main(String[] args) {
+        SpringApplication.run(AppStars.class);
     }
+
 }
