7bdcdc497ec4710a56e6b9774683e185d5f6361c
sunquan <sunquan5711@outlook.com>
2019-05-14 17:31:00 +0800

    切面已经可以用了

--- src/main/java/com/sunquan/glf/annotation/RedisTransaction.java
+++ src/main/java/com/sunquan/glf/annotation/RedisTransaction.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
  * 加在service层控制redis事务
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
 public @interface RedisTransaction {
 
--- src/main/java/com/sunquan/glf/annotation/RedisTransactionAspect.java
+++ src/main/java/com/sunquan/glf/annotation/RedisTransactionAspect.java
@@ -1,20 +1,28 @@
 package com.sunquan.glf.annotation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class RedisTransactionAspect {
 
     @Around("@annotation(redisTransaction)")
-    public void around(ProceedingJoinPoint joinPoint, RedisTransaction redisTransaction) throws Throwable {
+    public Object around(ProceedingJoinPoint joinPoint, RedisTransaction redisTransaction) throws Throwable {
 
+        logger.info("test");
         System.out.println("这里是切面中的方法");
-        joinPoint.proceed();
+        Object res = joinPoint.proceed();
+        System.out.println("这里是执行方法之后");
+        return res;
     }
 
+    private static Logger logger = LoggerFactory.getLogger(RedisTransactionAspect.class);
 
 }
